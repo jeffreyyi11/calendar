68846ac1f391a76d6569d192d08b38022fd61673
Jeffrey <jyi@PS-MminiDT.local>
2021-12-23 14:08:57 -0800

    update listobjects

--- components/minio_functions/getObjects.js
+++ components/minio_functions/getObjects.js
@@ -1,7 +1,22 @@
 //grab all objects(events) from bucket for community
 
 export default async () => {
-    const response = await fetch('/api/minio/getobjects');
-    let data = await response.json();
-    return data;
+    //fetch events for community
+    let data;
+    fetch('/api/Minio/getobjects')
+        .then(response => data = response.json())
+        .then(data => {
+            let filteredData = [];
+            for (let i = 0; i < data.length; i++) {
+                data[i]['name'] = data[i]['name'].split('/')[1]
+                let date = data[i]['lastModified'].slice(0,10).split('-');
+                let year = date[0];
+                let month = date[1];
+                let day = date[2];
+                data[i]['date'] = `${month}/${day}/${year}`;
+                filteredData.push(data[i]);
+            };
+            return filteredData;
+        })
+        .catch(error => console.log(error));
 }
